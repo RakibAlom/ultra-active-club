71a6c669d056720d227469551d212b010566f815
RakibAlom <rakibalom17@gmail.com>
2022-09-29 15:31:12 +0600

    Merriweather Google Font Added

--- src/components/ExerciseDetails/BreakTimes.js
+++ src/components/ExerciseDetails/BreakTimes.js
@@ -4,9 +4,9 @@ const BreakTimes = (props) => {
   const { handleBreakTimes } = props;
   return (
     <div>
-      <h4 className='pb-4'>Add A Break</h4>
+      <h4 className='pb-2'>Add A Break</h4>
       <div className='break-points py-3 py-md-3 px-5 px-md-2 px-md-3 rounded-3 d-flex justify-content-between'>
-        <div className="break-single-point rounded-5 p-1 p-md-2">
+        <div className="break-single-point rounded-5 p-1 p-md-2 d-md-none d-xl-block">
           <span onClick={() => handleBreakTimes(10)}>10s</span>
         </div>
         <div className="break-single-point rounded-5 p-1 p-md-2 active">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg mb-4">
-        <a className="navbar-brand" href="/">
-          Kingdom-of-Fantasy-Gym
+        <a className="navbar-brand fw-semibold" href="/">
+          <FontAwesomeIcon className='fs-4' icon={faDumbbell} /> Kingdom-of-Fantasy-Gym
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
